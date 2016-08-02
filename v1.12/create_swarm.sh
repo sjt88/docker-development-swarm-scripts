9aa4d6638fd8788b51f00f863f5bffedbce3f682
sjt88 <simonjtweed@googlemail.com>
2016-08-02 12:30:54 +0100

    bigger swarm cluster script

--- v1.12/create_swarm.sh
+++ v1.12/create_swarm.sh
@@ -4,6 +4,7 @@ echo "***********************************"
 docker-machine create -d virtualbox manager1 &
 docker-machine create -d virtualbox worker1 &
 docker-machine create -d virtualbox worker2
+docker-machine create -d virtualbox worker3
 
 echo "***********************************"
 echo "       initialising manager        "
@@ -16,3 +17,9 @@ echo "***********************************"
 export DOCKERSWARM_JOIN_CMD="$(docker-machine ssh manager1 docker swarm join-token worker | sed -n '2,4p')"
 docker-machine ssh worker1 "$DOCKERSWARM_JOIN_CMD"
 docker-machine ssh worker2 "$DOCKERSWARM_JOIN_CMD"
+docker-machine ssh worker3 "$DOCKERSWARM_JOIN_CMD"
+
+eval $(docker-machine env manager1)
+echo "***********************************"
+echo "    connected to swarm manager     "
+echo "***********************************"
