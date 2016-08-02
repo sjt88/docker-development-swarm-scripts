d7e185a4c0d03948d034b6c1f1bbd565121170a0
sjt88 <simonjtweed@googlemail.com>
2016-08-02 10:07:54 +0100

    minor fix

--- v1.12/create_swarm.sh
+++ v1.12/create_swarm.sh
@@ -8,11 +8,11 @@ docker-machine create -d virtualbox worker2
 echo "***********************************"
 echo "       initialising manager        "
 echo "***********************************"
-docker-machine ssh manager1 docker swarm init --advertise-addr $(docker-machine ip manager1)
+docker-machine ssh manager1 "docker swarm init --advertise-addr $(docker-machine ip manager1)"
 
 echo "***********************************"
 echo "     joining workers to swarm      "
 echo "***********************************"
 export DOCKERSWARM_JOIN_CMD="$(docker-machine ssh manager1 docker swarm join-token worker | sed -n '2,4p')"
-docker-machine ssh worker1 $DOCKERSWARM_JOIN_CMD
-docker-machine ssh worker2 $DOCKERSWARM_JOIN_CMD
+docker-machine ssh worker1 "$DOCKERSWARM_JOIN_CMD"
+docker-machine ssh worker2 "$DOCKERSWARM_JOIN_CMD"
