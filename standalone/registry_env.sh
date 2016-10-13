cb45a052994c448b3fc90d406c1ce504c925f217
Simon Tweed <simonjtweed@gmail.com>
2016-10-13 14:32:12 +0100

    improves & comments setup_registry

---
+++ standalone/registry_env.sh
@@ -0,0 +1,5 @@
+export HOST_IP="123.123.123.123"       # registry host IP
+export HOSTNAME="testhost"             # registry hostname
+export PORT=5000                       # registry port
+export CERTSD_FOLDER="$HOSTNAME:$PORT" # registry hostname:port
+export HOST_CERT_PATH="$(pwd)/test"    # /path/to/registry/ca.cert
--- standalone/setup_registry.sh
+++ standalone/setup_registry.sh
@@ -1,7 +1,8 @@
-export HOST_IP="123.123.123.123"
-export HOSTNAME="testhost"
-export CERTSD_FOLDER="testhost:5000"
-export HOST_CERT_PATH="$(pwd)/test"
+# sets up swarm machines to use a docker registry
+# - copies & installs registry ca cert
+# - adds registry hostname/ip to machines hosts file
+
+. ./registry_env.sh
 
 for machine_name in $(docker-machine ls | grep swarm- | awk '{print $1}'); do
   docker-machine ssh $machine_name "sudo mkdir /etc/docker/certs.d; sudo mkdir /etc/docker/certs.d/$CERTSD_FOLDER"
